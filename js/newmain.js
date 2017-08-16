5e2cca0476f09d052de5c253f75e5736d1cc25d7
root <keya5541@colorado.edu>
2017-08-15 21:24:34 -0700

    Changed features

--- js/newmain.js
+++ js/newmain.js
@@ -6,8 +6,7 @@ deviations = [];
 timediffs = [];
 testrunning = false; // the lack of camelcase is screwing with me, TODO <----
 
-var current = 0;
-var difference = 0;
+var tempTime = 0;
 var xVal = 0;
 var yVal = 0;
 var updateInterval = 100;
@@ -15,7 +14,6 @@ var dataLength = 50; // number of dataPoints visible at any point
 //var dps = []; // dataPoints
 var runNumber = 0;
 var counterNumber = 0;
-
 var baseData = {
 	    type: "spline", //try changing to column, area
 	    //toolTipContent: "{label}: {y} mm",
@@ -47,9 +45,9 @@ function beginTest() {
     key2 = $('#key2').val();
     $("div#status").html("Test ready, press assigned keys or click to begin.");
     $("div#Result").html("\
-        Tap Speed: 0 taps / 0 seconds<br>\
-        Stream Speed: 0 BPM<br>\
-        Unstable Rate: 0.\
+        Total Clicks: 0<br>\
+        Current Speed: 0 BPM<br>\
+        Unstable Rate: 0\
     ");
     std = 0;
     if (runNumber > 0) {
@@ -75,66 +73,27 @@ function endTest() {
     return;
 }
 
-function checkAcc(hit)
-{
-    current = (Date.now() - beginTime);
-    if (hit)
-    {
-        stepper += step;
-        difference = (current - (stepper));
-        checkOD(difference);
-    }
-    else
-    {
-        difference = (current - (stepper + step));
-        if (difference > 1000)
-        {
-            document.getElementById("hiterror").innerHTML = "";
-            document.getElementById("acc").src = "";
-        }
-    }
-}
 function checkOD(error)
 {
-    if (document.getElementById("acc").name != 300)
-    {
-        if (error >= 0) // hit too late
-        {
-            document.getElementById("hiterror").innerHTML = "Late";
-        }
-        else
-        {
-            // too early
-            document.getElementById("hiterror").innerHTML = "Early";
-        }
-    }
-    else
-    {
-        document.getElementById("hiterror").innerHTML = "";
-    }
     if (OD == 0)
     {
         if ((error <= 79.5) && (error >= -79.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 139.5) && (error >= -139.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 199.5) && (error >= -199.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -143,25 +102,21 @@ function checkOD(error)
     {
         if ((error <= 73.5) && (error >= -73.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 131.5) && (error >= -131.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 189.5) && (error >= -189.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -170,25 +125,21 @@ function checkOD(error)
     {
         if ((error <= 67.5) && (error >= -67.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 123.5) && (error >= -123.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 179.5) && (error >= -179.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -197,25 +148,21 @@ function checkOD(error)
     {
         if ((error <= 61.5) && (error >= -61.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 115.5) && (error >= -115.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 169.5) && (error >= -169.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -224,25 +171,21 @@ function checkOD(error)
     {
         if ((error <= 55.5) && (error >= -55.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 107.5) && (error >= -107.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 159.5) && (error >= -159.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -251,25 +194,21 @@ function checkOD(error)
     {
         if ((error <= 49.5) && (error >= -49.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 99.5) && (error >= -99.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 149.5) && (error >= -149.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -278,25 +217,21 @@ function checkOD(error)
     {
         if ((error <= 43.5) && (error >= -43.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 91.5) && (error >= -91.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 139.5) && (error >= -139.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -305,25 +240,21 @@ function checkOD(error)
     {
         if ((error <= 37.5) && (error >= -37.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 83.5) && (error >= -83.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 129.5) && (error >= -129.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -332,25 +263,21 @@ function checkOD(error)
     {
         if ((error <= 31.5) && (error >= -31.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 75.5) && (error >= -75.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 119.5) && (error >= -119.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -359,25 +286,21 @@ function checkOD(error)
     {
         if ((error <= 25.5) && (error >= -25.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 67.5) && (error >= -67.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 109.5) && (error >= -109.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -386,25 +309,21 @@ function checkOD(error)
     {
         if ((error <= 19.5) && (error >= -19.5))
         {
-            document.getElementById("acc").src = "img/hit300.png";
             document.getElementById("acc").name = 300;
             return;
         }
         else if ((error <= 59.5) && (error >= -59.5))
         {
-            document.getElementById("acc").src = "img/hit100.png";
             document.getElementById("acc").name = 100;
             return;
         }
         else if ((error <= 99.5) && (error >= -99.5))
         {
-            document.getElementById("acc").src = "img/hit50.png";
             document.getElementById("acc").name = 50;
             return;
         }
         else
         {
-            document.getElementById("acc").src = "img/hit0.png";
             document.getElementById("acc").name = 0;
             return;
         }
@@ -426,10 +345,14 @@ function update(click) {
         }
         clickTimes.push(Date.now());
         if (clickTimes.length > 1)
+        {
             timediffs.push(clickTimes[clickTimes.length - 1] - clickTimes[clickTimes.length - 2]);
-        if (clickTimes.length > 2) {
+        }
+        if (clickTimes.length > 2)
+        {
             var chart = $("#chartContainer").CanvasJSChart();
-            chart.options.data[runNumber].dataPoints.push({
+            chart.options.data[runNumber].dataPoints.push
+            ({
                 x: ((Date.now() - beginTime)/1000.0),
                 y: (Math.round((((clickTimes.length) / (Date.now() - beginTime) * 60000)/4) * 100) / 100)
             });
@@ -438,26 +361,24 @@ function update(click) {
     }
     else
     {
-        checkAcc(false);
         counterNumber = (counterNumber + 1) % 30;
-        streamtime = (Date.now() - beginTime)/1000;
-        if (timediffs.length < 2) {
+        if (timediffs.length < 4) {
             $("div#Result").html("\
-            Tap Speed: " + (clickTimes.length.toString() + " taps / " + streamtime.toFixed(3)) + " seconds.<br>\
-            Stream Speed: " + (Math.round((((clickTimes.length) / (Date.now() - beginTime) * 60000)/4) * 100) / 100).toFixed(2) + " BPM.<br>\
-            Unstable Rate: n/a.\ ");
+            Total Clicks: " + clickTimes.length.toString() + "<br>\
+            Current Speed: " + (Math.round(60000/(Date.now() - clickTimes[clickTimes.length-4])).toFixed(0)) + " BPM<br>\
+            Unstable Rate: n/a\
+        ");
         } else {
-            $("div#Result").html("\
-                Tap Speed: " + (clickTimes.length.toString() + " taps / " + streamtime.toFixed(3)) + " seconds.<br>\
-                Stream Speed: " + (Math.round((((clickTimes.length) / (Date.now() - beginTime) * 60000)/4) * 100) / 100).toFixed(2) + " BPM.<br>\
-                Unstable Rate: " + (Math.round(unstableRate * 100000) / 100000).toFixed(3) + "\ ");
+            $("div#Result").html(" Total Clicks: " + clickTimes.length.toString() + "<br>\
+                Current Speed: " + (Math.round(60000/(Date.now() - clickTimes[clickTimes.length-4])).toFixed(0)) + " BPM<br>\
+                Unstable Rate: " + (Math.round(unstableRate * 100000) / 100000).toFixed(3));
             if (counterNumber == 0) {
             var chart = $("#chartContainer").CanvasJSChart();
             chart.options.data[runNumber].dataPoints.push({
-                x: ((Date.now() - beginTime)/1000.0),
+                x: (Date.now() - beginTime)/1000.0,
                 y: (Math.round((((clickTimes.length) / (Date.now() - beginTime) * 60000)/4) * 100) / 100)
             });
-            chart.render();
+                chart.render();
             }
         }
     }
@@ -481,7 +402,6 @@ $(document).keypress(function(event)
                     updater = setInterval(function() { update(false); }, 16.6);
                     endTimer = setTimeout(function() { begin(); }, timeLimit * 1000);
                 default:
-                    checkAcc(true);
                     update(true);
                     break;
             }
@@ -505,7 +425,6 @@ $(document).mousedown(function(event)
                     updater = setInterval(function() { update(false); }, 16.6);
                     endTimer = setTimeout(function() { begin(); }, timeLimit * 1000);
                 default:
-                    checkAcc(true);
                     update(true);
                     break;
             }
